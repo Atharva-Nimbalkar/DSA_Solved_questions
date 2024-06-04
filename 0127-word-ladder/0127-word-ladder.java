907ca7c63c73056b61576b9bb2379906c2cc34d8
Atharva_Dev <113286938+Atharva-Nimbalkar@users.noreply.github.com>
2024-06-05 00:36:14 +0530

    Time: 92 ms (52.06%), Space: 46.3 MB (66.11%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -8,44 +8,44 @@ class Pair{
 }
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<String> queue = new LinkedList<>();
-        // converting to set as set remove() takes O(1) time while list remove() takes O(n)
-        HashSet<String> dict = new HashSet<>(wordList);
-        if(!dict.contains(endWord)){
-            return 0;
-        }
-        if(beginWord.equals(endWord)){
-            return 1;
-        }
-        
-        queue.add(beginWord);
-        int count = 1; // 1 for beginWord
-        while(queue.size()>0){
-            int size = queue.size();
-            // for all words at current level
-            for(int i=0; i<size; i++){
-                char[] word = queue.poll().toCharArray();
-                // we will replace each char one by one with [a,z] and check if it exists in our dictionary
-                for(int j=0; j<word.length; j++){
-                    char tmp = word[j];
-                    for(char c='a'; c<='z'; c++){
-                        word[j] = c;
-                        String newWord = new String(word); // next word after replacing jth character
-                        if(dict.contains(newWord)){
-                            if(newWord.equals(endWord)){
-                                return count + 1;
-                            }
-                            queue.add(newWord);
-                            dict.remove(newWord);
-                        }
+                // Creating a queue ds of type {word,transitions to reach ‘word’}.
+        Queue < Pair > q = new LinkedList < > ();
+        // BFS traversal with pushing values in queue 
+        // when after a transformation, a word is found in wordList.
+        q.add(new Pair(beginWord, 1));
+
+        // Push all values of wordList into a set
+        // to make deletion from it easier and in less time complexity.
+        Set < String > st = new HashSet <> (wordList);
+        st.remove(beginWord);
+        while (!q.isEmpty()) {
+            String word = q.peek().first;
+            int steps = q.peek().second;
+            q.remove();
+      
+            // we return the steps as soon as
+            // the first occurence of targetWord is found.
+            if (word.equals(endWord) == true) return steps;
+
+            // Now, replace each character of ‘word’ with char
+            // from a-z then check if ‘word’ exists in wordList.
+            for (int i = 0; i < word.length(); i++) {
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    char replacedCharArray[] = word.toCharArray();
+                    replacedCharArray[i] = ch;
+                    String replacedWord = new String(replacedCharArray);
+
+                    // check if it exists in the set and push it in the queue.
+                    if (st.contains(replacedWord) == true) {
+                        st.remove(replacedWord);
+                        q.add(new Pair(replacedWord, steps + 1));
                     }
-                    word[j] = tmp; // resetting to actual word for next iteration of inner for loop.
                 }
+
             }
-            // we have checked for all next words reachable from current queue, Hence
-            count += 1; 
         }
-        // reaching here means we have not found endWord, yet return 0
+        // If there is no transformation sequence possible
         return 0;
+
     }
 }
